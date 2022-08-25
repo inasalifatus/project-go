ad623c9e22f713acfb5c3c86bb8a14e9418f13ff
salma.inas <inas.salma@mncbank.co.id>
2022-08-25 20:02:00 +0700

    slice, pointer, struct

---
+++ pointer/pointer.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type People struct {
+	name   string
+	gender string
+}
+
+func main() {
+	persons := []*People{
+		{name: "Inas", gender: "P"},
+		{name: "Ricky", gender: "L"},
+		{name: "Thalia", gender: "P"},
+		{name: "Yudha", gender: "L"},
+		{name: "Clara", gender: "P"},
+		{name: "Agus", gender: "L"},
+		{name: "Yosef", gender: "L"},
+		{name: "Eka", gender: "L"},
+		{name: "Satrio", gender: "L"},
+		{name: "Rijal", gender: "L"},
+	}
+
+	printFriends := func(friends []*People) {
+		for _, value := range friends {
+			fmt.Println("Name:", value.name, ",", "Gender:", value.gender)
+		}
+	}
+
+	printFriends(persons)
+}
